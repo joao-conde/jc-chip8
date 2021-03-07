bd18b201612327a97c8cd43cef85b48e61233340
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-03-07 19:08:24 +0000

    some css, js as the API I wish it was

--- demo/index.js
+++ demo/index.js
@@ -9,36 +9,36 @@ const videoBuff = new DataView(image.data.buffer);
 
 (async () => {
     await wasm();
-    
-    const rom = await getROM("Pong.ch8")
-    
+
+    const rom = await getROM("Pong.ch8");
+
     const chip8 = new Chip8();
     chip8.load_rom(rom);
 
-    const clockFreq = 240 //Hz
-    const fps = 144 //FPS
+    const clockFreq = 500 //Hz
+    const fps = 60 //FPS
 
-    while (true) {
-        chip8.clock();
-        // render(chip8.vram());
-    }
+    const clock = function() {
+        this.clock
+    }.bind(chip8);
 
-    // window.setInterval(() => chip8.clock(), 1000 / clockFreq)
-    // window.setInterval(() => render(chip8.vram()), 1000 / fps)
+    const draw = function() {
+        const pixels = this.vram();
+        for (let i = 0, j = 0; i < pixels.length; i++, j += 4) videoBuff.setUint32(j, pixels[i] === 1 ? pixelSetColor : pixelUnsetColor)
+        ctx.putImageData(image, 0, 0)
+        ctx.drawImage(canvas, 0, 0)
+    }.bind(chip8);
+
+    window.setInterval(() => clock, 1000 / clockFreq)
+    window.setInterval(() => draw, 1000 / fps)
 })();
 
 function getROM(rom) {
     return new Promise(resolve => {
         const request = new XMLHttpRequest();
-        request.open("GET", `roms/${rom}`, true); 
+        request.open("GET", `roms/${rom}`, true);
         request.responseType = 'arraybuffer'
         request.onload = () => resolve(request.response)
         request.send()
     })
-}
-
-function render(vram) {
-    for (let i = 0, j = 0; i < vram.length; i++, j += 4) videoBuff.setUint32(j, vram[i] === 1 ? pixelSetColor : pixelUnsetColor)
-    ctx.putImageData(image, 0, 0)
-    ctx.drawImage(canvas, 0, 0)
 }

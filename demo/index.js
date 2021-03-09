baff719541c8e4a67038921c03f9a6d223bc0bcf
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-03-09 19:41:37 +0000

    prettier

--- demo/index.js
+++ demo/index.js
@@ -1,11 +1,11 @@
 import { default as wasm, Chip8 } from "./jc_chip8.js";
 
-const fps = 60 //FPS
-const clockFreq = 500 //Hz
-const pixelSetColor = 0xFFFFFFFF  //white
-const pixelUnsetColor = 0x000000FF //black
-const canvas = document.querySelector('canvas#main')
-const ctx = canvas.getContext('2d');
+const fps = 60; //FPS
+const clockFreq = 500; //Hz
+const pixelSetColor = 0xffffffff; //white
+const pixelUnsetColor = 0x000000ff; //black
+const canvas = document.querySelector("canvas#main");
+const ctx = canvas.getContext("2d");
 const image = ctx.createImageData(canvas.width, canvas.height);
 const videoBuff = new DataView(image.data.buffer);
 
@@ -13,25 +13,27 @@ const videoBuff = new DataView(image.data.buffer);
     await wasm();
     const rom = await getROM("Pong.ch8");
     const chip8 = new Chip8();
-	chip8.load_rom(rom);
-	window.setInterval(cycle.bind(chip8), 10);
+    chip8.load_rom(rom);
+    window.setInterval(cycle.bind(chip8), 10);
 })();
 
-
 async function getROM(rom) {
-	const response = await window.fetch(`roms/${rom}`);
-  	return await response.arrayBuffer();
+    const response = await window.fetch(`roms/${rom}`);
+    return await response.arrayBuffer();
 }
 
 function cycle() {
-	this.clock();
-	const pixels = this.vram();
-	console.log(pixels);
-	render(pixels);
+    this.clock();
+    const pixels = this.vram();
+    render(pixels);
 }
 
 function render(pixels) {
-    for (let i = 0, j = 0; i < pixels.length; i++, j += 4) videoBuff.setUint32(j, pixels[i] === 1 ? pixelSetColor : pixelUnsetColor)
-    ctx.putImageData(image, 0, 0)
-    ctx.drawImage(canvas, 0, 0)
+    for (let i = 0, j = 0; i < pixels.length; i++, j += 4)
+        videoBuff.setUint32(
+            j,
+            pixels[i] === 1 ? pixelSetColor : pixelUnsetColor
+        );
+    ctx.putImageData(image, 0, 0);
+    ctx.drawImage(canvas, 0, 0);
 }

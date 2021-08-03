b0f393a218ffaedfef3e33d5ce1dbb1aa7f2f2d5
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2021-08-03 23:52:46 +0100

    feat: wrapping sprites

--- src/chip8.rs
+++ src/chip8.rs
@@ -220,10 +220,12 @@ impl Chip8 {
     fn draw_sprite(&mut self, x0: usize, y0: usize, height: usize) {
         self.registers[0xF] = 0;
         for y in 0..height {
-            let sprite = self.ram[self.i as usize + y];
+            let sprite_line = self.ram[self.i as usize + y];
             for x in 0..8 {
-                let addr = (y0 + y) * SCREEN_PIXEL_WIDTH + x0 + x;
-                if (sprite & (0x80 >> x)) != 0 && addr < SCREEN_PIXEL_WIDTH * SCREEN_PIXEL_HEIGHT {
+                let xf = (x + x0) % SCREEN_PIXEL_WIDTH;
+                let yf = (y + y0) % SCREEN_PIXEL_HEIGHT;
+                let addr = yf * SCREEN_PIXEL_WIDTH + xf;
+                if (sprite_line & (0x80 >> x)) != 0 {
                     if self.vram[addr] == 1 {
                         self.registers[0xF] = 1;
                     }
